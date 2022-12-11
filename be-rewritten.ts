cc9434b95250c62f2ce47db4e8a24e935d0f8c6f
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-12-11 11:23:18 -0500

    Initial commit

--- be-rewritten.ts
+++ be-rewritten.ts
@@ -1,10 +1,19 @@
-import {BeWritten, virtualProps, proxyPropDefaults, actions} from 'be-written/be-written.js';
+import {BeWritten, 
+    virtualProps as BWVirtualProps, 
+    proxyPropDefaults as BWProxyPropDefaults, 
+    actions as BWActions
+} from 'be-written/be-written.js';
 import {register} from 'be-hive/register.js';
 import {define, BeDecoratedProps, DEMethods} from 'be-decorated/DE.js';
+import {ActionExt} from 'be-decorated/types';
 import {Actions, PP, PPE, VirtualProps, Proxy, ProxyProps, PPP} from './types';
+import { ProxyProps as BWPP } from 'be-written/types';
+import { StreamOrator } from '../stream-orator/StreamOrator';
 
 export class BeRewritten extends BeWritten{
-
+    override async getSet(pp: PP, so: StreamOrator, target: Element){
+        
+    }
 }
 
 const tagName = 'be-rewritten';
@@ -13,7 +22,25 @@ const ifWantsToBe = 'rewritten';
 
 const upgrade = '*';
 
+const virtualProps = [...BWVirtualProps];
 
+const proxyPropDefaults = {...BWProxyPropDefaults} as PPP;
+
+const actions = {...BWActions} as Partial<{[key in keyof Actions]: ActionExt<VirtualProps & BeDecoratedProps<VirtualProps, Actions>, Actions>}>
+
+define<VirtualProps & BeDecoratedProps<VirtualProps, Actions>, Actions>({
+    config: {
+        tagName,
+        propDefaults: {
+            ifWantsToBe,
+            upgrade,
+            virtualProps,
+            primaryProp: 'from',
+            proxyPropDefaults
+        },
+        actions,
+    }
+});
 
 register(ifWantsToBe, upgrade, tagName);
 
